6b4f39854ad619bd9aafbd3c927c3b3c309abcdc
Akira Komamura <akira.komamura@gmail.com>
2024-11-09 11:44:32 +0900

    style: Apply eslint

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 async function generateReadme(filename: string) {
-  const file = Bun.file(filename);
+  const file = Bun.file(filename)
   await Bun.write(file, `
 ![Number of stargazers by language](./generated/star-history.svg)
-`);
+`)
 }
 
-generateReadme("README.md");
+generateReadme('README.md')
