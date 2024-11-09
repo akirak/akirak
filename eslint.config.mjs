01554642decbbf3ee46da5633c2c3816fc34ad5d
Akira Komamura <akira.komamura@gmail.com>
2024-11-09 11:40:20 +0900

    chore: Set up eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,15 @@
-import antfu from "@antfu/eslint-config"
-import * as graphqlEslint from "@graphql-eslint/eslint-plugin"
+import antfu from '@antfu/eslint-config'
 
 export default antfu({
   stylistic: {
     indent: 2,
-    quotes: "double",
   },
   ignores: [
-    "dist",
+    '*.md',
   ],
   rules: {
-    "no-console": "off",
+    'no-console': 'off',
   },
 }, {
-  // This isn't working. Please check for updates in the plugin
-  files: ["**/*.ts"],
-  processor: graphqlEslint.processors.graphql,
-  languageOptions: {
-    parserOptions: {
-      schema: "./node_modules/@octokit/graphql-schema/schema.graphql",
-    },
-  },
+  files: ['*.ts'],
 })
